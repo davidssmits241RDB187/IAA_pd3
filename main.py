98e61c71a91dcb728718e92ce2cb96464d5245ca
davids <davids.gregors.smits@gmail.com>
2026-03-29 13:09:33 +0300

    first commit

---
+++ main.py
@@ -0,0 +1,30 @@
+
+
+def add_impulse_noise():
+    pass
+
+def add_even_noise():
+    pass
+
+def filter_via_average():
+    pass
+
+def filter_via_median():
+    pass
+
+def filter_via_gauss():
+    pass
+
+def load_image():
+    pass
+
+def exhibit_changes():
+    pass
+
+
+def main():
+    pass
+
+
+if __name__=="__main__":
+    main()
